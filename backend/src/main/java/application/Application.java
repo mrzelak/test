c87fb52b7ce9885e4e9a9e94d6a6fa652823f8f1
lys1k <lysikadrianna@gmail.com>
2022-04-08 16:43:54 +0200

    update Application

--- backend/src/main/java/application/Application.java
+++ backend/src/main/java/application/Application.java
@@ -1,15 +1,13 @@
 package application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args){
-		new SpringApplicationBuilder(Application.class).web(WebApplicationType.NONE).run(args);
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
