c4a5e6311da4d7a5ac9d06456cce26ad44ef2dfd
Adam <adamniemiec862@gmail.com>
2022-04-24 12:34:30 +0200

    Add cross

--- backend/src/main/java/application/controller/AuthController.java
+++ backend/src/main/java/application/controller/AuthController.java
@@ -13,10 +13,7 @@
 //import org.springframework.security.core.context.SecurityContextHolder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 //
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.*;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.http.ResponseEntity;
@@ -25,6 +22,7 @@
 //
 //@RestController
 //@RequestMapping("/authentication/")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 //public class AuthController {
 //
 //    @Autowired
