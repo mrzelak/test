60087a915cc32dc0c7141c870dd55fc06bb93fe3
jklimekk <asiaa.klimek@interia.pl>
2022-04-25 16:16:54 +0200

    added buttons

--- frontend/src/pages/authorized/Task/Details/view.js
+++ frontend/src/pages/authorized/Task/Details/view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import Button from 'components/Button';
 import { taskShape } from 'templates/DayTasks/shapes';
 
 const TaskDetailsView = ({ task }) => {
@@ -14,6 +15,20 @@ const TaskDetailsView = ({ task }) => {
         <h2>Deadline</h2>
         <p>{task.datetime}</p>
       </div>
+
+      <Button
+        onClick={() => console.log(`edit ${task.id}`)}
+        sx={{ marginTop: 20 }}
+      >
+        Edytuj zadanie
+      </Button>
+
+      <Button
+        onClick={() => console.log(`delete ${task.id}`)}
+        sx={{ marginTop: 20, marginLeft: 20 }}
+      >
+        Usuń zadanie
+      </Button>
     </Box>
   );
 };
