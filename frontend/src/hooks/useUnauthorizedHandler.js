3bf66f2322f8b9e1e523e4434ab25f7f1e1a1158
Michał Skwara <michal.skwara@fingoweb.com>
2022-05-10 20:33:38 +0200

    hotfix: clear token if unauthorized

--- frontend/src/hooks/useUnauthorizedHandler.js
+++ frontend/src/hooks/useUnauthorizedHandler.js
@@ -1,10 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import UserManager from 'managers/UserManager';
 
 const useUnauthorizedHandler = () => {
   const navigate = useNavigate();
 
   const handleUnauthorized = (err) => {
     if (err.response.data.error === 'Unauthorized') {
+      UserManager.clear();
       navigate('/public/login');
     }
   };
