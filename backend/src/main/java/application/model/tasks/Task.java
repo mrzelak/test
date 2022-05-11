32f71bb8ab37e43557751a81a57a0d9a4dbe0a1d
Adam <adamniemiec862@gmail.com>
2022-05-11 12:51:57 +0200

    KAP-125 optimise imports

--- backend/src/main/java/application/model/tasks/Task.java
+++ backend/src/main/java/application/model/tasks/Task.java
@@ -1,8 +1,6 @@
 package application.model.tasks;
 
 import application.Commons;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.util.ArrayList;
