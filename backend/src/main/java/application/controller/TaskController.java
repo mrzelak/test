f39b58b516b7609e7474d62442e5a96122064d8b
Kamil <kamilzubko@student.agh.edu.pl>
2022-05-13 13:47:05 +0200

    [KAP-115] Delete addTag and removeTag from TaskController

--- backend/src/main/java/application/controller/TaskController.java
+++ backend/src/main/java/application/controller/TaskController.java
@@ -1,6 +1,5 @@
 package application.controller;
 
-import application.model.tag.Tag;
 import application.model.tasks.Task;
 import application.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,16 +64,6 @@ public class TaskController {
     @GetMapping("/task/{startDate}/{endDate}")
     public List<Task> getTaskInGivenPeriodOfTime(@PathVariable String startDate, @PathVariable  String endDate) {
         return  taskService.getTasksInGivenPeriodOfTime(startDate, endDate);
-    }
-
-    @PutMapping("/task/{id}/tags")
-    Task addTag(@RequestBody Tag tag, @PathVariable Long id) {
-        return taskService.addTag(id, tag);
-    }
-
-    @DeleteMapping("/task/{id}/tags")
-    public void deleteTag(@RequestBody Tag tag, @PathVariable Long id) {
-        taskService.deleteTag(id, tag);
     }
 
     @GetMapping("/task/{tagName}")
